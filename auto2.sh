900f01529cd5def40bf3884698a659c3df47bc97
44maker <101875427+44maker@users.noreply.github.com>
2023-09-11 22:32:31 +0800

    Create auto2.sh

---
+++ auto2.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+set +e
+
+# Python2 pip
+wget https://bootstrap.pypa.io/pip/2.7/get-pip.py
+python2 get-pip.py
+clear
+sleep 3
+
+# Pip2 and pip3
+pip3 install --upgrade setuptools && python3 -m pip install --upgrade pip && pip2 install --upgrade setuptools && python2 -m pip install --upgrade pip
+clear
+echo -e "\nPip2 and pip3 are upgraded\n"
+sleep 3
+
+sleep 2
+#go
+sudo apt-get install -y golang
+echo 'export GOPATH=/root/go-workspace' >> ~/.zshrc
+echo 'export GOROOT=/usr/local/go' >> ~/.zshrc
+echo 'export PATH=$PATH:$GOROOT/bin/:$GOPATH/bin' >> ~/.zshrc
+source ~/.zshrc
+
+#tmux
+sleep 1
+cat > ~/.tmux.conf <<-'EOF'
+#tmux attach 如果无分离终端则新建
+new-session
+# split panes using | and -
+bind | split-window -h
+bind - split-window -v
+unbind ' " '
+unbind %
+bind -r k select-pane -U # 绑定k为↑
+bind -r j select-pane -D # 绑定j为↓
+bind -r h select-pane -L # 绑定h为←
+bind -r l select-pane -R # 绑定l为→
+# reload config file (change file location to your the tmux.conf you want to use)
+bind r source-file ~/.tmux.conf; display-message "Config reloaded.."
+bind V source-file ~/.tmux/layout1         #ctrl b + shift v => change
+EOF
+
+mkdir ~/.tmux
+sleep 1
+cat > ~/.tmux/layout1 <<-'EOF'
+selectp -t 0 # select the first (0) pane
+splitw -v -p 50 # split it into two halves
+selectp -t 1 # select the new, second (1) pane
+splitw -h -p 50 # split it into two halves
+selectp -t 0 # go back to the first pane
+EOF
+#ad
+go install github.com/ropnop/kerbrute@latest
+sleep 2
+git clone https://github.com/ThePorgs/impacket/ 
+pip3 install -r requirements.txt 
+python3 setup.py install
+pip install bloodyAD
+clear
