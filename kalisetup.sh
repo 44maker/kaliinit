57a7d551435d3bcac229b8ad7df2a2cc761bab04
44maker <101875427+44maker@users.noreply.github.com>
2023-09-12 06:58:10 +0800

    Create kalisetup.sh

---
+++ kalisetup.sh
@@ -0,0 +1,3 @@
+#!bin/bash
+wget https://raw.githubusercontent.com/44maker/kaliinit/main/auto.sh && chmod +x auto.sh && sudo bash auto.sh
+wget https://raw.githubusercontent.com/44maker/kaliinit/main/auto2.sh && chmod +x auto2.sh && bash auto2.sh
