49a684bfb96099c5a7e27a4adfccbe9bb7ed55bc
44maker <101875427+44maker@users.noreply.github.com>
2023-09-14 13:30:15 +0800

    Update kalisetup.sh

--- kalisetup.sh
+++ kalisetup.sh
@@ -1,3 +1,7 @@
 #!bin/bash
 wget https://raw.githubusercontent.com/44maker/kaliinit/main/auto.sh && chmod +x auto.sh && sudo bash auto.sh
 wget https://raw.githubusercontent.com/44maker/kaliinit/main/auto2.sh && chmod +x auto2.sh && bash auto2.sh
+
+sleep 2
+rm -rf get-pip.py auto.sh auto2.sh
+clear
